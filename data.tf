52ddcf394807eac407318ea556e2a766a6a46be3
mahesh <mahesh050917@gmail.com>
2026-03-03 08:30:40 +0530

    terraform vpc modules

--- data.tf
+++ data.tf
@@ -1,3 +1,11 @@
 data "aws_availability_zones" "available" {
   state = "available"
 }
+
+data "aws_vpc" "default_vpc" {
+cidr_block = "172.31.0.0/16"
+}
+
+data "aws_route_table" "default_routetable" {
+  route_table_id = rtb-0322ed59b804dd591
+}
---
+++ peering.tf
@@ -0,0 +1,18 @@
+resource "aws_vpc_peering_connection" "peering" {
+  peer_vpc_id = data.aws_vpc.default_vpc.id
+  vpc_id = aws_vpc.main.id
+  auto_accept = true
+}
+
+resource "aws_route" "peering_route" {
+  destination_cidr_block = aws_vpc.main.cidr_block
+  route_table_id = aws_route_table.Private_rt_table.id
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
+}
+
+
+resource "aws_route" "peering_route" {
+  destination_cidr_block = data.aws_vpc.default_vpc.cidr_block
+  route_table_id = data.aws_route_table.default_routetable.id
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
+}
